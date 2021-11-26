0fcdcc5e397ad7ec7b78af6223e02d9e8723637d
unknown <hshanto707@gmail.com>
2021-11-26 21:19:35 +0600

    initial commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div>
+      {/* HEADER */}
+
+      {/* HERO */}
+
+      {/* PRODUCTS */}
+
+      {/* BANNER */}
+
+      {/* BRANDS */}
+
+      {/* TESTIMONIALS */}
+
+      {/* SERVICES */}
+
+      {/* NEWSLETTER */}
+
+      {/* FOOTER */}
+    </div>
+  );
+};
+
+export default Home;
