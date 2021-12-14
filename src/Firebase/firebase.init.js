0d7d75d5934a104d252fd12e49bc2d9b10c604b0
unknown <hshanto707@gmail.com>
2021-12-14 19:44:33 +0600

    single product page created

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWgWcrYuBFMhaRYexTzLdYtMiSoja9Das",
+  authDomain: "crafteza-hhs.firebaseapp.com",
+  projectId: "crafteza-hhs",
+  storageBucket: "crafteza-hhs.appspot.com",
+  messagingSenderId: "978117975859",
+  appId: "1:978117975859:web:fd46371e37e65d30ae2846"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Container, NavLink } from "react-bootstrap";
+import { faHeart, faStar } from "@fortawesome/free-solid-svg-icons";
+import "./ProductDetail.css";
 
 const ProductDetail = () => {
   return (
-    <div>
-      
+    <div style={{ paddingTop: "100px" }}>
+      <Container className="product-container">
+        <div>
+          <img
+            className="product-img"
+            src="https://www.pngitem.com/pimgs/m/508-5082434_dining-table-top-view-png-dining-room-chair.png"
+            alt=""
+          />
+        </div>
+        <div className="product-info">
+          <p>Dining {">"} Chair</p>
+          <h1>Odium Counter Stool</h1>
+          <div className="rating mb-3">
+            <FontAwesomeIcon className="marked" icon={faStar} />
+            <FontAwesomeIcon className="marked" icon={faStar} />
+            <FontAwesomeIcon className="marked" icon={faStar} />
+            <FontAwesomeIcon className="marked" icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </div>
+          <NavLink href="sdf" className="link d-inline p-0">
+            <FontAwesomeIcon icon={faHeart} />
+            <p className="ms-2 d-inline">Add To Wishlist</p>
+          </NavLink>
+          <hr />
+          <p>
+            Take casual dining to new heights with the Odium bar stool. Admire
+            how the dark bronze-tone powder coat metal finish and planked
+            looking back and seat reflect the popular rustic, industrial style.
+            Decorative details include metal rivets and curved metal design. A
+            convenient swivel feature also adjusts the seat height, perfect for
+            flexibility.
+          </p>
+          <hr />
+          <div className="d-flex align-items-center">
+            <p className="offer-price">$199</p>
+            <p className="reg-price ms-2">$240</p>
+          </div>
+          <div className="fs-3 d-flex gap-2 py-3">
+            <p>QTY</p>
+            <input type="number" className="quantity" name="" id="" min="1" />
+            <input type="button" className="button round px-3" value="Add To Cart" />
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
